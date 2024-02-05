58bdb91e5b5ac2d815d989ba9e21e6dce96c68fa
mouyox21 <anibamouad@gmail.com>
2024-02-05 11:07:41 +0100

    Pusging Day 11 - Node

---
+++ DAY11/Challenge/mathOperations.js
@@ -0,0 +1,22 @@
+function addNumbers(num1, num2) {
+    return num1 + num2;
+   }
+
+function SubNumbers(num1, num2) {
+    return num1 - num2;
+   }
+
+function MultiNumbers(num1, num2) {
+    return num1 * num2;
+   }
+
+   function DeviNumbers(num1, num2) {
+    return num1 / num2;
+   }
+
+
+   console.log(addNumbers(3,3))
+   console.log(SubNumbers(6, 4));
+   console.log(MultiNumbers(5, 8));
+   console.log(DeviNumbers(9, 3));
+
